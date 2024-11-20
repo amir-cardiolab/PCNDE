5ccbc88f37539301b45fa278ee0638ec1a3017f8
Hunor Csala <40268779+TsChala@users.noreply.github.com>
2024-11-20 11:53:52 -0700

    Delete src/lda_funcs.jl

--- src/lda_funcs.jl
+++
@@ -1,7 +0,0 @@
-#=
-The functions required for the Length Difference and Angle Difference (LDA) loss function 
-=#
-
-normed_ld(a,b) = abs(norm(a)-norm(b))/(norm(a)+norm(b))
-cosine_similarity(a, b) = dot(a,b) / (norm(a) * norm(b))
-cosine_distance(a, b) = (1 - cosine_similarity(a, b))/2
